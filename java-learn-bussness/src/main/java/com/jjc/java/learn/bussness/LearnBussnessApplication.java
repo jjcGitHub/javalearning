c867a620a8c3f677f53ad24656a4c0ebf7e818a6
jjcGitHub <1226196941@qq.com>
2021-03-25 20:46:47 +0800

    1

---
+++ java-learn-bussness/src/main/java/com/jjc/java/learn/bussness/LearnBussnessApplication.java
@@ -0,0 +1,20 @@
+package com.jjc.java.learn.bussness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * @program: demo
+ * @description: ${描述}
+ * @author: Skipper
+ * @create: 2021-03-25 11:46
+ **/
+
+@SpringBootConfiguration
+public class LearnBussnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnBussnessApplication.class,args);
+        System.out.println("ok");
+    }
+}
